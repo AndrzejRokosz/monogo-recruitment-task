e866f12a2298853f3215672c98ea0e6207094148
AndrzejRokosz <andrzej_ro@wp.pl>
2018-12-21 14:22:07 +0100

    basic search by name added

--- src/View/ListView.js
+++ src/View/ListView.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles/listView.css'
+import { TextField } from 'material-ui'
+import { unifyString } from '../utils/unifyString'
 
 const NUMBER_OF_USERS_TO_BE_FETCHED = 50
 
@@ -8,6 +10,7 @@ class ListView extends React.Component {
         data: '',
         currentPage: 1,
         dataPerPage: 5,
+        searchedText: ''
     }
     componentDidMount() {
         fetch(`https://randomuser.me/api?results=${NUMBER_OF_USERS_TO_BE_FETCHED}`)
@@ -15,6 +18,10 @@ class ListView extends React.Component {
             .then(data => this.setState({
                 data: data.results
             }))
+    }
+
+    onSearchedTextChangeHandler = (event) => {
+        this.setState({ searchedText: event.target.value })
     }
 
     getCurrentPageData = () => {
@@ -49,24 +56,34 @@ class ListView extends React.Component {
                     <div className='inner-data-container'>
                         <div
                             className='inner-data-container__currentPage-header'
-                            
+
                         >{`Page: ${this.state.currentPage}`}
                         </div>
+                        <TextField
+                            floatingLabelText="Search user by last name"
+                            onChange={this.onSearchedTextChangeHandler}
+                        />
+
                         {this.getCurrentPageData() ?
                             this.getCurrentPageData().map ?
-                                this.getCurrentPageData().map(user => (
-                                    <div
-                                        className='single-user-container'
-                                        key={user.email}
-                                    >
-                                        <img className='user-container__image' src={user.picture.large} alt='user' />
-                                        <div>{`${user.name.first} ${user.name.last} Age: ${user.dob.age}`}</div>
-                                        <a href={user.email}>{user.email}</a>
-                                        <div>{`Address: `}</div>
-                                        <div>{`${user.location.street}, ${user.location.city} `} </div>
-                                        <div>{`Phone: ${user.phone}`}</div>
-                                    </div>
-                                ))
+                                this.getCurrentPageData()
+                                    .filter(user => (
+                                        unifyString(user.name.last)
+                                            .includes(unifyString(this.state.searchedText))
+                                    ))
+                                    .map(user => (
+                                        <div
+                                            className='single-user-container'
+                                            key={user.email}
+                                        >
+                                            <img className='user-container__image' src={user.picture.large} alt='user' />
+                                            <div>{`${user.name.last} ${user.name.first}  Age: ${user.dob.age}`}</div>
+                                            <a href={user.email}>{user.email}</a>
+                                            <div>{`Address: `}</div>
+                                            <div>{`${user.location.street}, ${user.location.city} `} </div>
+                                            <div>{`Phone: ${user.phone}`}</div>
+                                        </div>
+                                    ))
                                 : 'Something went wrong !'
                             : 'Loading data ...'
                         }
---
+++ src/utils/unifyString.js
@@ -0,0 +1,9 @@
+
+
+export const unifyString = string => (
+    string
+      .toLowerCase()
+      .replace(/\s/g, '')
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, "")
+  )
